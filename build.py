18052c3f6c9575eff82148221617e6efc8a12e56
rohanhg91 <rohanhgupta91@gmail.com>
2017-08-28 08:51:20 +0000

    Done

--- build.py
+++ build.py
@@ -5,10 +5,28 @@ def solution_asc(dic):
     '''
     Enter your code here
     '''
+    op = []
+    for i in range (0, len(dic)):
+        for k, v in dic.iteritems():
+            output = (k,v)
+            op.append(output)
+        return op
 
 
 def solution_desc(dic):
     '''
     Enter your code here
     '''
+    op = []
+    for i in range (0, len(dic)):
+        for k, v in dic.iteritems():
+            output = (k,v)
+            op.append(output)
+        op1 = op[::-1]
+        return op1
 
+'''
+dic = {2: 21, 1: 11, 0: 12, 9: 211, 4: 55}
+a = solution_desc(dic)
+print a
+'''
